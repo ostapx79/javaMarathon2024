430b08f4c13ed251ba98777dbf04154b3f3cfafb
Constantin Galus <ostapx79@gmail.com>
2024-05-27 19:56:06 +0300

    Задача четвертая и пятая сделано, тесты проходят

--- src/main/java/day1/Task4.java
+++ src/main/java/day1/Task4.java
@@ -2,6 +2,12 @@ package day1;
 
 public class Task4 {
     public static void main(String[] args) {
-
+        int yearStart = 1980;
+        int yearFinish = 2020;
+        // int i = yearStart;
+        while (yearStart <= yearFinish) {
+            System.out.println("Олимпиада " + yearStart + " года");
+            yearStart += 4;
+        }
     }
 }
--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
+        int yearStart = 1980;
+        int yearFinish = 2020;
 
+        for (; yearStart <= yearFinish; yearStart += 4) {
+            System.out.println("Олимпиада " + yearStart + " года");
+        }
     }
 }
