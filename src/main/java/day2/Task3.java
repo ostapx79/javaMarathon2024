ec56d93aaac3768750f43e26925a1825101ed41b
ostapx79 <ostapx79@gmail.com>
2024-05-29 00:02:01 +0300

    сделал задачу /day2/Task3.java

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,20 @@
 package day2;
 
-public class Task3 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Task3 {
+    /*
+    3. Реализовать программу №2, используя цикл while.
+     */
+    final static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a >= b) System.out.println("Некорректный ввод");
+        while (a < b) {
+            if (a % 5 == 0 && a % 10 != 0) System.out.print(a + " ");
+            a++;
+        }
     }
 }
