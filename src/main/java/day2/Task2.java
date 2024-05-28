aa6dd5008ad0f9ed7585c39ffe1d7dea4fbed1c2
ostapx79 <ostapx79@gmail.com>
2024-05-28 23:53:14 +0300

    сделал задачу /day2/Task2.java

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,5 +1,7 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     /*
     2. Есть два числа, которые задаются пользователем через консоль (назовем эти два числа a и b). Используя
@@ -19,7 +21,17 @@ public class Task2 {
     int a = scanner.nextInt();
     int b = scanner.nextInt();
      */
+    final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a >= b) System.out.println("Некорректный ввод");
+        for (; a < b; a++) {
+            if (a % 5 == 0 && a % 10 != 0) {
+                System.out.print(a + " ");
+            }
+        }
 
     }
 }
